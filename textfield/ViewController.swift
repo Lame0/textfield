06dae0202164cb5371c63d8ea78044b26a2c0671
Damian <damianna070@gmail.com>
2022-09-07 10:06:32 -0600

    mvp

--- textfield/ViewController.swift
+++ textfield/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txt: UITextField!
+    
+    var x=""
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    
     }
 
+    
+    
+    @IBAction func butt(_ sender: Any) {
+        //the resign function dissmises the key board
+        txt.resignFirstResponder()
+        x=txt.text ?? "Yo"
+        label.text=x
+        
+    }
+    
 
 }
 
